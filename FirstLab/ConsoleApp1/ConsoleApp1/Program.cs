98774a0b877ece97a7e0d7842ec84b72f9a7db7c
s28250 <ladnyiartem@gmail.com>
2024-03-05 13:23:38 +0100

    My first commit

---
+++ FirstLab/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please answer a number");
+            if (int.TryParse(Console.ReadLine(), out int value)) {
+                if (value > 0)
+                {
+                    Console.WriteLine("The value is positive one");
+                }
+                else if(value < 0)
+                {
+                    Console.WriteLine("The value is a negative one");
+                }
+                else
+                {
+                    Console.Write("Thw value is 0");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+            Console.WriteLine("Press to exit");
+            Console.ReadKey();
+        }
+    }
+}
