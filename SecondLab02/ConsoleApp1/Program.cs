fff14a0f4121872dc6edf67ef686ff7c72484669
s28250 <ladnyiartem@gmail.com>
2024-03-12 13:15:52 +0100

    first commid to feature-max

--- SecondLab02/ConsoleApp1/Program.cs
+++ SecondLab02/ConsoleApp1/Program.cs
@@ -15,12 +15,32 @@
 
             return sum / numbers.Length;
         }
+        static int FindMaximumValue(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Array is null or empty", nameof(numbers));
+            }
+
+            int max = numbers[0];
+            foreach (var number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            return max;
+        }
 
         static void Main(String[] args)
         {
             int[] arr = new[] { 1, 23, 4, 2 };
             double avg = CalculateAverage(arr);
+            int max = FindMaximumValue(arr);
             Console.WriteLine(avg);
+            Console.WriteLine(max);
             Console.WriteLine("lalalalal");
         }
  }
