64db3aeac15cb1f88acae2028239a30f9ea04e8f
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-04-22 23:59:49 +0200

    lab05

---
+++ WebApplication2/WebApplication2/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    // Manually specify the HTTPS port based on your launchSettings.json
+    app.Urls.Add("https://localhost:7170");
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
